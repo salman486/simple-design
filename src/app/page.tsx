892a4b317ac4a9e969752e62d9f47214788bd39a
salman486 <msalman2k1@gmail.com>
2024-10-01 02:41:48 +0500

    Setup signup page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/sign-up");
+  return (
+    <div className="flex  justify-center min-h-screen mt-10">
+      <h1 className="text-5xl text-red-500">Landing Page</h1>
+    </div>
+  );
+}
